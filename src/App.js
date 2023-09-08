3294e88f096ab4ddda4c037f3704c36aa8579f0c
xdethx <gittim.geldim.git@gmail.com>
2023-09-09 02:21:29 +0300

    commit2
    com2

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import { Link,Route,Routes } from 'react-router-dom';
 import MoviesPage from './components/pages/MoviesPage'
 import React, { Component } from 'react';
 import 'semantic-ui-css/semantic.min.css';
-import {
-  Container,
-  Divider,
- 
 
-  
- 
-  Image,
-  List,
-  Menu,
-  Segment,
-} from 'semantic-ui-react'
 
 
 import Footer from './components/Footer'
